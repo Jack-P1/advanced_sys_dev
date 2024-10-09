9c99d2c5eb92409852577a319bcd521f2e0e9574
Jack Pilbeam (Student) <jack2.pilbeam@live.uwe.ac.uk@csctcloud.prxhn32zsyjupl12zde3wlfkch.cwx.internal.cloudapp.net>
2024-10-09 17:02:27 +0100

    refactor ref counter as templated class

--- worksheet1/reference_counter.hpp
+++ worksheet1/reference_counter.hpp
@@ -1,17 +1,51 @@
 #ifndef REFERENCE_COUNTER_H
 #define REFERENCE_COUNTER_H
 
+template<class T>
 class reference_counter {
-    public:
-        reference_counter();
-        void increment();
-        void decrement();
-        unsigned int getCount() const;
-        reference_counter& operator= (reference_counter const& r);
-
     private:
-        unsigned int ref_count;
-        unsigned int* ref_count_ptr;
+        T* ref_ptr = nullptr;
+        unsigned int* ref_count = nullptr;
+    
+    public:
+        reference_counter()
+        {
+            ref_count = new unsigned int(0);
+        }
+
+        reference_counter(T* ptr)
+        {
+            ref_ptr = ptr;
+            ref_count = new unsigned int(1); 
+        }
+
+        reference_counter(const reference_counter& object){
+            this->ref_ptr = object.ref_ptr;
+            this->ref_count = object.ref_count;
+
+            (*this->ref_count)++;
+        }
+        
+        T* operator->() const
+        {
+                return this->ref_ptr;
+        }
+
+        unsigned int getCount()
+        {
+            return *ref_count;
+        }
+
+        ~reference_counter(){
+            (*ref_count)--;
+
+            if((*ref_count) == 0){
+                std::cout << "Ref count equals 0, freeing memory..." << std::endl;
+                delete ref_ptr;
+                delete ref_count;
+            }
+        }
+
 };
 
 #endif
